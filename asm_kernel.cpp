7a98d43a58533b8efa6e03cb857175a47f23232b
Rene van Oostrum <Rene.vanOostrum@amd.com>
2021-03-04 10:28:34 -0600

    test program with trivial kernel

---
+++ asm_kernel.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <vector>
+#include <hip/hip_runtime.h>
+
+__global__ void kernel(int* args, int size){
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  if(idx < size){
+    args[idx] = idx;
+  }
+}
+
+
+int main(){
+  constexpr int size = 64;
+  
+  int* args;
+  hipMalloc(&args, size * sizeof(int));
+  hipLaunchKernelGGL(kernel, 1, size, 0, 0, args, size);
+  hipDeviceSynchronize();
+  std::vector<int> results(size);
+  hipMemcpy(results.data(), args, size * sizeof(int), hipMemcpyDeviceToHost);
+  for(auto& r: results){
+    printf("%d  ", r);
+  }
+  printf("\n");
+}
